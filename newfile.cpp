09a098af3d4361c77221b9b5d596e6a6a45d15f6
Sanket Kumar <154404394+sanketkrpandey@users.noreply.github.com>
2024-06-15 09:58:50 +0530

    Update newfile.cpp

--- newfile.cpp
+++ newfile.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 void solve(int a,int b ,int c)
 {
-  int n=max(a,b),sum;
-  n=max(n,c);
+  int n=max({a,b,c}),sum=0;
+  
   for( int i=0;i<n;i++)
     {
       sum=sum+i;
@@ -16,7 +16,7 @@ int main()
 {
   int t;
   cin>>t;
-  vector<int> num(3);
+  vector<vector<int>> num(t,vector<int>(3));
   for(int i=0;i<t;i++)
     {
       cin>>nums[i][0]>>nums[i][1]>>nums[i][2];

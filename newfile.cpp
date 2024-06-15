5e7a71a3d438f78d7d88a5dca240b6073913cf88
Sanket Kumar <154404394+sanketkrpandey@users.noreply.github.com>
2024-06-15 10:01:14 +0530

    Update newfile.cpp

--- newfile.cpp
+++ newfile.cpp
@@ -4,7 +4,7 @@ void solve(int a,int b ,int c)
 {
   int n=max({a,b,c}),sum=0;
   
-  for( int i=0;i<n;i++)
+  for( int i=0;i<=n;i++)
     {
       sum=sum+i;
     }
@@ -16,7 +16,7 @@ int main()
 {
   int t;
   cin>>t;
-  vector<vector<int>> num(t,vector<int>(3));
+  vector<vector<int>> nums(t,vector<int>(3));
   for(int i=0;i<t;i++)
     {
       cin>>nums[i][0]>>nums[i][1]>>nums[i][2];

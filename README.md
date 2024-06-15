05c8b03f3aae2f95f95772c68308c6ee5932e5a0
Sanket Kumar <154404394+sanketkrpandey@users.noreply.github.com>
2024-06-15 08:47:07 +0530

    CREATE newfile.cpp

--- README.md
+++
@@ -1,5 +0,0 @@
-## hello 
-this is a readme file.
-
-
-adding a new line for this readme. enjoying learning git and github!!
---
+++ newfile.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(int a,int b ,int c)
+{
+  int n=max(a,b),sum;
+  n=max(n,c);
+  for( int i=0;i<n;i++)
+    {
+      sum=sum+i;
+    }
+  cout<<sum<<endl;
+}
+int main()
+{
+  int t;
+  cin>>t;
+  vector<int> num(3);
+  for(int i=0;i<t;i++)
+    {
+      cin>>nums[i][0]>>nums[i][1]>>nums[i][2];
+    }
+  for(int i=0;i<t;i++)
+    {
+      solve(nums[i][0],nums[i][1],nums[i][2]);
+    }
+  return 0;
+}
